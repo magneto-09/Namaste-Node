b6f6a8c1a5d11f5b8df5c37cc38acbfac751acf6
Piyush Sharma <92093386+magneto-09@users.noreply.github.com>
2025-04-05 21:25:52 +0530

    Saving dynamic data into DB via APIs (#9)

--- src/model/user.js
+++ src/model/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     age: { type: Number, required: true },
-    email:{type:String, required:true}
+    email: { type: String, required: true }
 })
 
 
@@ -19,9 +19,9 @@ const User = mongoose.model("user", userSchema);  // model -> JS wrapper around 
     */
 
 // instance(s) of model --> document(s) -> creating manually
-const newUser = new User({ name: "Piyush", age: 23, email:"spiyush1721@gmail.com" }); 
-const newUser1 = new User({name:"Piyush", age:23, email:"noobm2992@gmail.com"}); 
-const newUser2 = new User({name:"Akshay Saini", age:32, email:"akshayMarch09@gmail.com"});
+const newUser = new User({ name: "Piyush", age: 23, email: "spiyush1721@gmail.com" });
+const newUser1 = new User({ name: "Piyush", age: 23, email: "noobm2992@gmail.com" });
+const newUser2 = new User({ name: "Akshay Saini", age: 32, email: "akshayMarch09@gmail.com" });
 
 
 // funcn that'll insert the user into db

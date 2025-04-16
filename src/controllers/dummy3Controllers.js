8899b5a70a799c6e98c42cc62a292176eb1fd28c
magneto-09 <noobm2992@gmail.com>
2025-04-16 20:53:32 +0530

    Authentication, JWT, Cookies

--- src/controllers/dummy3Controllers.js
+++ src/controllers/dummy3Controllers.js
@@ -46,7 +46,7 @@ const signupController = async (req, res, next) => {
   // checks. that's why dummy3 collection in the DB is vulnerable like others.
 };
 
-// update Profile --> PATCH  --> route -> /basePath/profile1/:email --> dynamic
+// update Profile --> PATCH  --> route -> /basePath/profile1/:email --> dynamic --> DB level checks only
 const updateProfileController = async (req, res, next) => {
   try {
     // Manual check to ensure email passed via params is truly an email.
@@ -128,8 +128,143 @@ const updateProfileController1 = async (req, res, next) => {
   }
 };
 
+// ------------ Ep 23 - (Authentication, JWT, cookies) 🚀🚀🚀 -----------------------------------
+const { dummy3LoginValidation } = require("../helper/validation");
+
+// if creds are valid send some raw random information through cookies.
+const loginController = async (req, res, next) => {
+  try {
+    dummy3LoginValidation(req.body); // API level checks.
+
+    const { email, password } = req.body;
+    const ifExists = await Dummy3?.findOne({ email }); // returns document if present else undefined.
+
+    if (!ifExists) throw new Error("Invalid credentials");
+    else {
+      if (password !== ifExists?.password)
+        throw new Error("Invalid credentials");
+      else {
+        // means credentails are valid so sends some random information via cookies.
+        console.log("Login successful.");
+        res.cookie(
+          "random",
+          "sending some random info. to see how cookies works",
+          {
+            httpOnly: true, // makes the cookie invisible for the user
+            // but rest of the flow would be same. check Node S02 part 02 - ep-10 in the doc.
+            // path: "/dummy4",  // cookie will only be sent to /dummy4 route & their sub-routes.
+            // although even after set, you'll still see all the cookies in POSTMAN cookies UI section.
+            // but on the browser, the 'random' cookie (the name of cookie that we're sending from here)
+            // will be sent to /dummy4 route & their sub-routes.🚩🚩🚩🚩
+          }
+        );
+
+        return res.status(200).json({
+          message: "Logged In",
+          userDetails: ifExists,
+        });
+      }
+    }
+  } catch (error) {
+    console.log("Error Occured!!!!!!");
+    next(error); // default error middleware.
+  }
+};
+
+// cookieParser middleware has been added globally in the app.js to get the cookies from req.cookie.
+const getCookieController = (req, res, next) => {
+  try {
+    const cookies = req.cookies;
+    console.log(cookies);
+    return res.status(200).json({
+      cookies,
+    });
+  } catch (error) {
+    console.log("Error Occured!!!!!!");
+    next(error); // default error middleware.
+  }
+
+  // created a dummy route and this controller just to show how to read cookies in the controller.
+};
+
+// let's create a new login controller that sends a actual JWT via cookies & update profile controller
+// that will verify the user via tokens before updating.
+
+const JWT = require("jsonwebtoken");
+
+const loginController1 = async (req, res, next) => {
+  try {
+    dummy3LoginValidation(req.body); // API level checks.
+
+    const { email, password } = req.body;
+
+    const ifExists = await Dummy3?.findOne({ email }); // returns document if present else undefined.
+
+    if (!ifExists) throw new Error("Invalid credentials");
+    else {
+      if (password !== ifExists?.password)
+        throw new Error("Invalid credentials");
+      else {
+        // means credentails are valid so sends JWT token via cookies.
+        // 1 -- generate a JWT token and sends the existing user's id as payload while generating token.
+        const token = JWT.sign({ _id: ifExists?._id }, process.env.JWT_SECRET);
+
+        // 2 -- send the token through cookies.
+        res.cookie("token", token, {
+          httpOnly: true, // makes cookie invisible to user only.
+          path: "/dummy3", // cookie will be available only for /dummy3 and its subroutes.
+        });
+
+        console.log("Login successful.");
+
+        return res.status(200).json({
+          message: "Logged In",
+        });
+      }
+    }
+  } catch (error) {
+    console.log("Error Occured!!!!!!");
+    next(error); // default error middleware.
+  }
+};
+
+const getProfileController = async (req, res, next) => {
+  try {
+    // 3 -- extract the token from the req.cookies (needs cookie-parser)
+
+    // const cookies = req.cookies;
+    // console.log(cookies);
+
+    const { token } = req.cookies; // extracting the token.
+    // console.log(token);
+
+    if (!token) throw new Error("Update Not Allowed.");
+
+    // 4 -- Validate the token. Hence, validating the user.
+    const decodedMessage = JWT.verify(token, process.env.JWT_SECRET);
+    // if got verified successfully then only we'll be able to see user's id else we'll get an error.
+
+    // console.log(decodedMessage);
+
+    const data = await Dummy3?.findById(decodedMessage?._id);
+
+    return res.status(200).json({
+      data,
+    });
+  } catch (error) {
+    console.log("Error Occured!!!!!!");
+    next(error); // default error middleware.
+  }
+};
+
+// --------------------------------------------------------------------------------------------------
+
 module.exports = {
   signupController,
   updateProfileController,
   updateProfileController1,
+  loginController,
+  getCookieController,
+  loginController1,
+  getProfileController,
 };
--- src/routes/dummy3Routes.js
+++ src/routes/dummy3Routes.js
@@ -4,6 +4,10 @@ const {
   signupController: dummy3SignupController,
   updateProfileController: dummy3UpdateProfileController,
   updateProfileController1: dummy3UpdateProfileController1,
+  loginController: dummy3LoginController,
+  getCookieController: dummy3GetCookieController,
+  loginController1: dummy3LoginController1,
+  getProfileController: dummy3GetProfileController
 } = require("../controllers/dummy3Controllers");
 
 const router = express.Router(); // router instance
@@ -34,6 +38,20 @@ router.patch("/profile/:email", dummy3UpdateProfileController); // dynamic route
 router.patch("/profile1/:email", dummy3UpdateProfileController1);
 
 // ************************************************************************************************
+
+// ------------ Ep 23 - (Authentication, JWT, cookies) 🚀🚀🚀 -----------------------------------
+// creates a login API that sends some random information via cookies.
+router.post("/login", dummy3LoginController);
+
+router.get("/getCookie", dummy3GetCookieController); // dummy API to get the cookie for future routes.
+
+// let's create a new login API that sends a actual JWT via cookies & update profile API
+// that will verify the user via tokens before updating.
+router.post("/login1", dummy3LoginController1);
+
+router.get("/profile", dummy3GetProfileController);
+
+// --------------------------------------------------------------------------------------------------
 
 module.exports = {
   router,
